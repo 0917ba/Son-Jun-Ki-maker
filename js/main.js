5c248e9f0c323e67e48ae505995d5266e11647f7
0917ba <0917ba@naver.com>
2022-05-20 20:39:32 +0900

    saying added
    saying added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 const b = document.getElementById("submit");
 b.addEventListener("click", makeBoard);
 
+const btn = document.getElementsByClassName("btn_image");
+
+let clickSaying = new Array("맞다고 해!", "가까운 창문으로 가세요", "숨 참아!", "호오옹", "나는 준기!", "이거 모르면 사람 아니죠?");
+
 function makeBoard() {
     let a1 = parseInt(document.getElementById("txt1").value);
     let a2 = parseInt(document.getElementById("txt2").value);
@@ -12,14 +16,23 @@ function makeBoard() {
         tableEle += "<tr>";
         for(let j = 0; j < a1; j++) {
             let imageCnt = 2; 
-            let randN = makeRandInt(0, imageCnt - 1);
-            let currentImg = arrImage[randN];
-            tableEle += `<td><img src=${currentImg} width='120' height='160'></td>`;
+            let randIntTable = makeRandInt(0, imageCnt - 1);
+            let currentImg = arrImage[randIntTable];
+            tableEle += `<td><button type="button" class="btn_image"><img src=${currentImg} class="image">`;
         }
         tableEle += "</tr>";
     }
     tableEle += "</tr>";
     document.getElementById("board").innerHTML = tableEle;
+
+    for(let i = 0; i < btn.length; i++) {
+        btn[i].addEventListener("click", say);
+    }
+
+    function say() {
+        let randIntSaying = makeRandInt(0, clickSaying.length - 1);
+        alert(clickSaying[randIntSaying]);
+    }
 
     function makeRandInt(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
