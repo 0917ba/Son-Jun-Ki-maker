0c8d044451be67e7feaff3a85a4dabbdee335537
0917ba <0917ba@naver.com>
2022-05-20 15:56:18 +0900

    design, random
    background added, text designed, random added

--- js/main.js
+++ js/main.js
@@ -1,17 +1,27 @@
 const b = document.getElementById("submit");
-        b.addEventListener("click", makeBoard);
-        
-        function makeBoard() {
-            let a1 = parseInt(document.getElementById("txt1").value);
-            let a2 = parseInt(document.getElementById("txt2").value);
-            let tableEle = "<table>";
-            for(let i = 0; i < a1; i++) {
-                tableEle += "<tr>";
-                for(let j = 0; j < a2; j++) {
-                    tableEle += "<td><img src='img/jungi.png' width='120' height='160'></td>";
-                }
-                tableEle += "</tr>";
-            }
-            tableEle += "</tr>";
-            document.getElementById("board").innerHTML = tableEle;
+b.addEventListener("click", makeBoard);
+
+function makeBoard() {
+    let a1 = parseInt(document.getElementById("txt1").value);
+    let a2 = parseInt(document.getElementById("txt2").value);
+    let tableEle = "<table>";
+
+    let arrImage = new Array("img/jungi-1.png", "img/jungi-2.jpg");
+
+    for(let i = 0; i < a2; i++) {
+        tableEle += "<tr>";
+        for(let j = 0; j < a1; j++) {
+            let imageCnt = 2; 
+            let randN = makeRandInt(0, imageCnt - 1);
+            let currentImg = arrImage[randN];
+            tableEle += `<td><img src=${currentImg} width='120' height='160'></td>`;
         }
+        tableEle += "</tr>";
+    }
+    tableEle += "</tr>";
+    document.getElementById("board").innerHTML = tableEle;
+
+    function makeRandInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+}
