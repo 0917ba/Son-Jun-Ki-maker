5e0e953ad3230d3fc60bdfff2165bd17cbe14b56
0917ba <0917ba@naver.com>
2022-06-15 15:53:52 +0900

    6/15(2)
    디자인 수정

--- js/main.js
+++ js/main.js
@@ -8,27 +8,31 @@ let clickSaying = new Array("맞다고 해!", "가까운 창문으로 가세요"
 function makeBoard() {
     let a1 = parseInt(document.getElementById("txt1").value);
     let a2 = parseInt(document.getElementById("txt2").value);
-    let tableEle = "<table>";
+    if(!(Number.isInteger(a1)) || !(Number.isInteger(a2)) || a1 <= 0 || a2 <= 0) {
+        alert("1 이상의 정수를 입력해주세요");
+    } else {
+        let tableEle = "<div id='table'><table>";
 
-    let arrImage = new Array("img/jungi-1.png", "img/jungi-2.jpg", "img/jungi-3.jpg");
+        let arrImage = new Array("img/jungi-1.png", "img/jungi-2.jpg", "img/jungi-3.jpg");
 
-    for(let i = 0; i < a2; i++) {
-        tableEle += "<tr>";
-        for(let j = 0; j < a1; j++) {
-            let imageCnt = arrImage.length; 
-            let randIntTable = makeRandInt(0, imageCnt - 1);
-            let currentImg = arrImage[randIntTable];
-            tableEle += `<td><button type="button" class="btn_image"><img src=${currentImg} class="image">`;
+        for(let i = 0; i < a2; i++) {
+            tableEle += "<tr>";
+            for(let j = 0; j < a1; j++) {
+                let imageCnt = arrImage.length; 
+                let randIntTable = makeRandInt(0, imageCnt - 1);
+                let currentImg = arrImage[randIntTable];
+                tableEle += `<td><button type="button" class="btn_image"><img src=${currentImg} class="image">`;
+            }
+            tableEle += "</tr>";
         }
-        tableEle += "</tr>";
+        tableEle += "</tr></div>";
+        document.getElementById("board").innerHTML = tableEle;
+        document.getElementById("info").remove();
+        for(let i = 0; i < btn.length; i++) {
+            btn[i].addEventListener("click", say);
+        }
     }
-    tableEle += "</tr>";
-    document.getElementById("board").innerHTML = tableEle;
-
-    for(let i = 0; i < btn.length; i++) {
-        btn[i].addEventListener("click", say);
-    }
-
+    
     function say() {
         let randIntSaying = makeRandInt(0, clickSaying.length - 1);
         alert(clickSaying[randIntSaying]);
